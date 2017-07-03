ee9cb574623782ea4527bbf462045835aa3051dd
gcame <gabiact15@gmail.com>
2017-07-03 08:32:41 -0300

    Add repositório de produto no pacote persistencia.implementação

---
+++ VendasOnlineSystem/src/main/java/persistencia/implementacao/RepositorioProduto.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia.implementacao;
+
+import dao.DaoManagerHiber;
+import java.util.List;
+import negocio.Produto;
+import persistencia.RepositorioGenerico;
+
+/**
+ *
+ * @author gcame
+ */
+public class RepositorioProduto implements RepositorioGenerico <Produto> {
+
+    @Override
+    public void inserir(Produto t) {
+        DaoManagerHiber.getInstance().persist(t);
+    }
+
+    @Override
+    public void atualizar(Produto t) {
+        DaoManagerHiber.getInstance().update(t);
+    }
+
+    @Override
+    public void apagar(Produto t) {
+        DaoManagerHiber.getInstance().delete(t); 
+    }
+
+    @Override
+    public Produto recuperar(int id) {
+        try{
+            return (Produto) DaoManagerHiber.getInstance().recover("from Produto where id=" + id);
+        } 
+        catch(IndexOutOfBoundsException in){
+        return null;
+               }
+    }
+    @Override
+    public List<Produto> recuperarTodos() {
+        return DaoManagerHiber.getInstance().recoverSQL("from Produto");
+    }
+    
+}
